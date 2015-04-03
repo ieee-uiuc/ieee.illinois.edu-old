a499d8eea48d6fc8d46c0c837d0ed82482907414
anitgandhi <anitgandhi@gmail.com>
2015-04-03 02:07:26 -0500

    fix js on front page test

--- js/test.js
+++ js/test.js
@@ -26,64 +26,55 @@ $(document).ready(function()
         }
     });
 
-});
-
-
-(function (window, document) {
-var menu = document.getElementById('menu'),
+    var menu = document.getElementById('menu'),
     WINDOW_CHANGE_EVENT = ('onorientationchange' in window) ? 'orientationchange':'resize';
 
-function toggleHorizontal() {
-    [].forEach.call(
-        document.getElementById('menu').querySelectorAll('.custom-can-transform'),
-        function(el){
-            el.classList.toggle('pure-menu-horizontal');
-        }
-    );
-};
+	function toggleHorizontal() {
+	    [].forEach.call(
+	        document.getElementById('menu').querySelectorAll('.custom-can-transform'),
+	        function(el){
+	            el.classList.toggle('pure-menu-horizontal');
+	        }
+	    );
+	};
 
-function toggleMenu() {
-    // set timeout so that the panel has a chance to roll up
-    // before the menu switches states
-    if (menu.classList.contains('open')) {
-        setTimeout(toggleHorizontal, 500);
-    }
-    else {
-        toggleHorizontal();
-    }
-    menu.classList.toggle('open');
+	function toggleMenu() {
+	    // set timeout so that the panel has a chance to roll up
+	    // before the menu switches states
+	    if (menu.classList.contains('open')) {
+	        setTimeout(toggleHorizontal, 500);
+	    }
+	    else {
+	        toggleHorizontal();
+	    }
+	    menu.classList.toggle('open');
 
-    // toggle the mobile menu display attribute
-    var myNav = $(".my-navbar");
-    if (myNav.css("display") == "none")
-        myNav.css("display", "block")
-    else
-        myNav.css("display", "none");
+	    // toggle the mobile menu display attribute
+	    var myNav = $(".my-navbar");
+	    if (myNav.css("display") == "none")
+	        myNav.css("display", "block")
+	    else
+	        myNav.css("display", "none");
 
 
-    document.getElementById('toggle').classList.toggle('x');
+	    document.getElementById('toggle').classList.toggle('x');
 
-    // when the menu is opened on mobile it should also set the color as solid
-    setTimeout(function(){menu.classList.toggle('solid-nav');} , 500);
+	    // when the menu is opened on mobile it should also set the color as solid
+	    setTimeout(function(){menu.classList.toggle('solid-nav');} , 500);
 
-    // on menu close it's changing back to transparent before it closes
-};
+	    // on menu close it's changing back to transparent before it closes
+	};
 
-function closeMenu() {
-    if (menu.classList.contains('open')) {
-        toggleMenu();
-    }
-}
+	function closeMenu() {
+	    if (menu.classList.contains('open')) {
+	        toggleMenu();
+	    }
+	}
 
-document.getElementById('toggle').addEventListener('click', function (e) {
-    toggleMenu();
+	document.getElementById('toggle').addEventListener('click', function (e) {
+	    toggleMenu();
+	});
+
+	window.addEventListener(WINDOW_CHANGE_EVENT, closeMenu);
+
 });
-
-window.addEventListener(WINDOW_CHANGE_EVENT, closeMenu);
-
-window.onresize = function(event)
-{
-    console.log(event);
-}
-
-})(this, this.document);
