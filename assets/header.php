8543021b9b4866768245b6698a369ecbb66a0710
Anit Gandhi <anitgandhi@gmail.com>
2015-07-22 22:22:40 -0500

    clean up header.php logic

--- assets/header.php
+++ assets/header.php
@@ -1,15 +1,14 @@
 <?php 
 	$path = parse_url($_SERVER['HTTP_REFERER'], PHP_URL_PATH);
 
-	$arr = Array("/about/contact/" => "Contact",
-				 "/about/" => "About",
+	$arr = Array("/about/" => "About",
 				 "/news/" => "News",
 				 "/projects/" => "Projects",
 				 "/spark/" => "Spark",
 				 "/awards/" => "Awards",
-				 "/corporate/" => "Corporate Relations",
-				 "/join/" => "Join",
-				 "/" => "Home");
+				 "/corporate/" => "Corporate",
+				 "/contact/" => "Contact",
+				 "/join/" => "Join");
 
 	foreach($arr as $folder => $target)
 	{
@@ -22,8 +21,21 @@
 		}
 	}
 
-?>
+	// Generates a menu item based on the $section argument, referring to the global $arr
+	function genMenuItem($section)
+	{
+		global $arr, $found;
 
+		// Whether the current one is active
+		$active = ($found == $section ? ' navbar-link-active' : '');
+
+		// We want Join to be bold
+		$join = ($section == "Join" ? '<b>' . $section . '</b>' : $section);
+
+		return '<a href="' . array_search($section, $arr) . '"class="pure-menu-link navbar-link' . $active . '">'. $join . '</a>';
+	}
+
+?>
 
 <div class="pure-u-1 pure-u-md-1-4">
 	<div class="pure-menu">
@@ -35,117 +47,11 @@
 <div class="pure-u-1 pure-u-md-3-4 flex-nav">
 	<div class="pure-menu pure-menu-horizontal navbar-menu menu-can-transform">
 		<ul class="pure-menu-list">
-			<li class="pure-menu-item ieee-menu-item">
-				<?php
-
-				echo "<a href=\"/about/\" class=\"pure-menu-link navbar-link";
-
-				if ($found == "About")
-					echo " navbar-link-active\">About</a>\n<hr class=\"navbar-active\" />";
-
-				else
-					echo "\">About</a>"
-
-				?>
-			</li>
-
-			<li class="pure-menu-item ieee-menu-item">
-				<?php
-
-				echo "<a href=\"/news/\" class=\"pure-menu-link navbar-link";
-
-				if ($found == "News")
-					echo " navbar-link-active\">News</a>\n<hr class=\"navbar-active\" />";
-
-				else
-					echo "\">News</a>"
-
-				?>
-			</li>	
-
-			<li class="pure-menu-item ieee-menu-item">
-				<?php
-
-				echo "<a href=\"/projects/\" class=\"pure-menu-link navbar-link";
-
-				if ($found == "Projects")
-					echo " navbar-link-active\">Projects</a>\n<hr class=\"navbar-active\" />";
-
-				else
-					echo "\">Projects</a>"
-
-				?>
-			</li>
-
-			<li class="pure-menu-item ieee-menu-item">
-				<?php
-
-				echo "<a href=\"/spark/\" class=\"pure-menu-link navbar-link";
-
-				if ($found == "Spark")
-					echo " navbar-link-active\">Spark</a>\n<hr class=\"navbar-active\" />";
-
-				else
-					echo "\">Spark</a>"
-
-				?>
-			</li>
-
-			<li class="pure-menu-item ieee-menu-item">
-				<?php
-
-				echo "<a href=\"/awards/\" class=\"pure-menu-link navbar-link";
-
-				if ($found == "Awards")
-					echo " navbar-link-active\">Awards</a>\n<hr class=\"navbar-active\" />";
-
-				else
-					echo "\">Awards</a>"
-
-				?>
-			</li>
-
-			<li class="pure-menu-item ieee-menu-item">
-				<?php
-
-				echo "<a href=\"/corporate/\" class=\"pure-menu-link navbar-link";
-
-				if ($found == "Corporate Relations")
-					echo " navbar-link-active\">Corporate</a>\n<hr class=\"navbar-active\" />";
-
-				else
-					echo "\">Corporate</a>"
-
-				?>
-			</li>
-
-			<li class="pure-menu-item ieee-menu-item">
-				<?php
-
-				echo "<a href=\"/about/contact/\" class=\"pure-menu-link navbar-link";
-
-				if ($found == "Contact")
-					echo " navbar-link-active\">Contact</a>\n<hr class=\"navbar-active\" />";
-
-				else
-					echo "\">Contact</a>"
-
-				?>
-			</li>
-
-			<li class="pure-menu-item ieee-menu-item">
-				<?php
-
-				echo "<a href=\"/join/\" class=\"pure-menu-link navbar-link";
-
-				if ($found == "Join")
-					echo " navbar-link-active\"><b>Join</b></a>\n<hr class=\"navbar-active\" />";
-
-				else
-					echo "\"><b>Join</b></a>"
-
-				?>
-			</li>
+			<?php 
+				foreach($arr as $folder => $target) {
+					echo '<li class="pure-menu-item ieee-menu-item">' . genMenuItem($target) . '</li>';
+				}
+			?>
 		</ul>
 	</div>
 </div>
