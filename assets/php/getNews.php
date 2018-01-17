e970dfff439d7a9676174538de4eead9ac9b1e93
Dhruv Patel <akido2398@gmail.com>
2018-01-17 17:05:07 -0600

    Debugging

--- assets/php/getNews.php
+++ assets/php/getNews.php
@@ -1,13 +1,13 @@
 <?php
 
-//require_once("mysql_credentials.php");
+require_once("mysql_credentials.php");
 
 // Connect to the mysql server, and check if it was successful
 $con = mysqli_connect($mysqli_server, $mysqli_username, $mysqli_password, $mysqli_db);
 if (!$con)
 {
 	$ret = array("success" => false,
-				 "message" => "<h4>Could not load the news items. Please try refreshing the page.</h4>",
+				 "message" => "<h4>Could not load news items. Please try refreshing the page.</h4>",
 				 "error" => "Database connection error."
 				 );
 	die(json_encode($ret));
